e5e73a22296ce72bd748b304c1677b6e0ef73669
Greg Shanks <greg.shanks@gmail.com>
2020-02-06 11:20:30 +0000

    Added tests for Fighter

--- src/test/java/FighterTest.java
+++ src/test/java/FighterTest.java
@@ -1,15 +1,49 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class FighterTest {
 
     Fighter fighter;
+    Weapon weapon;
+    Treasure treasure;
 
     @Before
     public void setUp() {
         fighter = new Fighter("Minsc", 70, 10);
+        weapon = new Weapon("Axe", 5, 1);
+        treasure = new Treasure("The Black Ruby of Voldesad", "A gem of great value.", 100);
     }
 
+    @Test
+    public void canGetName() {
+        assertEquals("Minsc", fighter.getName());
+    }
 
+    @Test
+    public void canSetName() {
 
+    }
+
+    @Test
+    public void canGetHP() {
+        assertEquals(70, fighter.getHP());
+    }
+
+    @Test
+    public void canSetHP() {
+
+    }
+
+    @Test
+    public void canGetDamage() {
+        assertEquals(10, fighter.getDamage());
+    }
+
+    @Test
+    public void canSetDamage() {
+
+    }
 
 }
